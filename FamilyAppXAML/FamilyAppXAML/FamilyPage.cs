855e36e64e9af839d41173288140d2fa3644d111
estremn <estremn@gmail.com>
2017-02-27 12:12:49 -0600

    Initial Push

---
+++ FamilyAppXAML/FamilyAppXAML/FamilyPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FamilyAppXAML
+{
+    public class FamilyPage : ContentPage
+    {
+        public FamilyPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Scale = 1.5, FontAttributes = FontAttributes.Bold, Text = "My Family" },
+
+                    new Label { Scale = .8, Text = "I don't know half of them half of what I should like, "
+                    + "and like half of them half as well as they deserve." },
+
+                    new Label { Scale = 1, FontAttributes = FontAttributes.Bold,  Text = "Tammy Estrem" },
+                }
+            };
+        }
+    }
+}
